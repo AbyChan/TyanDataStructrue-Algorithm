94666fc58d48dbd3e0a1e0fd4cbcce19fe4f74ce
vtch99 <crystal.tyan@gmail.com>
2015-03-10 10:26:51 +0800

    update

---
+++ Algorithm/AboutDigital/Multiply-Strings.py
@@ -0,0 +1,17 @@
+2015年 03月 10日 星期二 10:22:39 CST
+
+class Solution:
+
+    # @param num1, a string
+
+    # @param num2, a string
+
+    # @return a string
+
+    def multiply(self, num1, num2):
+
+        rst = str( long(num1) * long(num2) )
+
+        return rst
+
+        
